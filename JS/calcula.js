b0979f6bb98bbcd9fd84e8fc50b1cb5c7b6d1732
alessandra <alessandraraissa04@gmail.com>
2023-05-10 16:00:56 -0300

    ver2

--- JS/calcula.js
+++ JS/calcula.js
@@ -1,8 +1,8 @@
 function calculo(){
-    var v1 = parseFloat(document.getElementById('val1').value);
-    var v2 = parseFloat(document.getElementById('val2').value);
-    var op = document.getElementById('opera').value;
-    var res;
+    let v1 = parseFloat(document.getElementById('val1').value);
+    let v2 = parseFloat(document.getElementById('val2').value);
+    let op = document.getElementById('opera').value;
+    let res;
 
     if((isNaN(v1)) || (isNaN(v2))){
         alert("INVÁLIDO! Somente números");
@@ -26,5 +26,32 @@ function calculo(){
             res = v1/v2;
             alert(res);
             break;
+    }
+}
+
+function lightMode(){
+    window.document.getElementById('light').style.display = 'none';
+    window.document.getElementById('dark').style.display = 'flex';
+    window.document.querySelector('body').style.backgroundColor = 'white';
+    window.document.querySelector('body').style.color = 'black';
+}
+
+function darkMode(){
+    window.document.getElementById('dark').style.display = 'none';
+    window.document.getElementById('light').style.display = 'flex';
+    window.document.querySelector('body').style.backgroundColor = 'black';
+    window.document.querySelector('body').style.color = 'white';
+    window.document.getElementById('alert').style.color = 'blue';
+}
+
+function nameVal(){
+    let name = nam.nome.value;
+    let texto = window.document.getElementById('text');
+
+    if (name == "") {
+        document.getElementById('alert').style.display = 'flex';
+    } else {
+        document.getElementById('alert').style.display = 'flex';
+        TextDecoder.innerHTML = '<h1>Bem vindo</h1>' + name + '<h1> !</h1>';
     }
 }
